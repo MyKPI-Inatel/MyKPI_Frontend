c92f77c58196a5736f18091c087a39e279c97377
leogalisse <leonardogalisse@gmail.com>
2024-10-04 19:18:51 -0300

    fix: fix sidebar on change route

--- my-kpi/src/components/sidebar.tsx
+++ my-kpi/src/components/sidebar.tsx
@@ -8,12 +8,15 @@ import {
   PartyPopper,
   Rocket,
   ScrollText,
-} from 'lucide-react';
-import { Fragment, useState } from 'react';
+} from "lucide-react";
+import { Fragment, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Sidebar() {
-  const [isSelected, setIsSelected] = useState('inicio');
   const [isMenuOpen, setIsMenuOpen] = useState(true);
+  const location = useLocation();
+
+  const getIsSelected = (path: string) => location.pathname === path;
 
   return (
     <Fragment>
@@ -29,65 +32,45 @@ export default function Sidebar() {
           </button>
           <div className="flex flex-col space-y-5">
             <a
-              onClick={() => {
-                setIsSelected('inicio');
-              }}
-              className={`flex space-x-2 p-4 rounded-lg cursor-pointer ${isSelected === 'inicio' ? 'bg-blue-500 text-white' : 'text-black'}`}
+              href="/"
+              className={`flex space-x-2 p-4 rounded-lg cursor-pointer ${getIsSelected("/") ? "bg-blue-500 text-white" : "text-black"}`}
             >
               <House />
               <span>Início</span>
             </a>
             <a
-              onClick={() => {
-                setIsSelected('celebracoes');
-              }}
-              className={`flex space-x-2 p-4 rounded-lg cursor-pointer ${isSelected === 'celebracoes' ? 'bg-blue-500 text-white' : 'text-black'}`}
+              className={`flex space-x-2 p-4 rounded-lg cursor-pointer ${getIsSelected("/celebracoes") ? "bg-blue-500 text-white" : "text-black"}`}
             >
               <PartyPopper />
               <span>Celebrações</span>
             </a>
             <a
-              onClick={() => {
-                setIsSelected('desenvolvimento');
-              }}
-              className={`flex space-x-2 p-4 rounded-lg cursor-pointer ${isSelected === 'desenvolvimento' ? 'bg-blue-500 text-white' : 'text-black'}`}
+              className={`flex space-x-2 p-4 rounded-lg cursor-pointer ${getIsSelected("/desenvolvimento") ? "bg-blue-500 text-white" : "text-black"}`}
             >
               <Rocket />
               <span>Desenvolvimento</span>
             </a>
             <a
-              onClick={() => {
-                setIsSelected('objetivos');
-              }}
-              className={`flex space-x-2 p-4 rounded-lg cursor-pointer ${isSelected === 'objetivos' ? 'bg-blue-500 text-white' : 'text-black'}`}
+              className={`flex space-x-2 p-4 rounded-lg cursor-pointer ${getIsSelected("/objetivos") ? "bg-blue-500 text-white" : "text-black"}`}
             >
               <Goal />
               <span>Objetivos</span>
             </a>
             <a
-              onClick={() => {
-                setIsSelected('gamificacao');
-              }}
-              className={`flex space-x-2 p-4 rounded-lg cursor-pointer ${isSelected === 'gamificacao' ? 'bg-blue-500 text-white' : 'text-black'}`}
+              className={`flex space-x-2 p-4 rounded-lg cursor-pointer ${getIsSelected("/gamificacao") ? "bg-blue-500 text-white" : "text-black"}`}
             >
               <Database />
               <span>Gamificação</span>
             </a>
             <a
-              onClick={() => {
-                setIsSelected('desempenho');
-              }}
-              className={`flex space-x-2 p-4 rounded-lg cursor-pointer ${isSelected === 'desempenho' ? 'bg-blue-500 text-white' : 'text-black'}`}
+              className={`flex space-x-2 p-4 rounded-lg cursor-pointer ${getIsSelected("/desempenho") ? "bg-blue-500 text-white" : "text-black"}`}
             >
               <ScrollText />
               <span>Desempenho</span>
             </a>
             <a
-              onClick={() => {
-                setIsSelected('configuracoes');
-              }}
-              className={`flex space-x-2 p-4 rounded-lg cursor-pointer ${isSelected === 'configuracoes' ? 'bg-blue-500 text-white' : 'text-black'}`}
-              href='/config'
+              className={`flex space-x-2 p-4 rounded-lg cursor-pointer ${getIsSelected("/config") ? "bg-blue-500 text-white" : "text-black"}`}
+              href="/config"
             >
               <Bolt />
               <span>Configurações</span>
