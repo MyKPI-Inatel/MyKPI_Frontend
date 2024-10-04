0dccd27d6786d119fd2d89a192386934b40fa8a8
leogalisse <leonardogalisse@gmail.com>
2024-10-04 19:06:24 -0300

    feat: add logout

--- my-kpi/src/components/navbar.tsx
+++ my-kpi/src/components/navbar.tsx
@@ -1,9 +1,24 @@
-import { Bell, CircleHelp, CircleUser, HeartHandshake, Megaphone } from 'lucide-react';
+import {
+  Bell,
+  CircleHelp,
+  CircleUser,
+  HeartHandshake,
+  LogOut,
+  Megaphone,
+} from "lucide-react";
 
 export function Navbar() {
+  function logout() {
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("userEmail")
+    localStorage.removeItem("username")
+    localStorage.removeItem("usertype")
+    localStorage.removeItem("userId")
+  }
+  
   return (
     <div className="flex w-full col-span-10 justify-between px-10 py-5 items-center border-b shadow-sm shadow-white">
-      <div className='flex w-14'>
+      <div className="flex w-14">
         <img src="/src/assets/logo.png" alt="Logo" />
       </div>
       <div className="flex space-x-5 items-center">
@@ -11,6 +26,9 @@ export function Navbar() {
         <Megaphone />
         <HeartHandshake />
         <Bell />
+        <a href="/login" onClick={logout}>
+          <LogOut />
+        </a>
         <div className="flex items-center justify-center space-x-2">
           <CircleUser size={40} strokeWidth={1} />
           <div className="flex flex-col">
