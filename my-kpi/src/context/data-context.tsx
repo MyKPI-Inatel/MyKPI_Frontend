9d654f9047085ae8816205055365cdc6bfdf1188
matheusjulidori <matheus_julidori@yahoo.com.br>
2024-10-16 20:42:55 -0300

    Feat: Spinner loading

--- my-kpi/src/context/data-context.tsx
+++ my-kpi/src/context/data-context.tsx
@@ -1,75 +1,77 @@
-import { createContext, useCallback, useState } from "react";
-import { Organizations } from "../components/config/columns";
-import { Departments } from "../components/config/department/columns";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { env } from "../lib/env";
+import { createContext, useCallback, useState } from 'react';
+import { Organizations } from '../components/config/columns';
+import { Departments } from '../components/config/department/columns';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { env } from '../lib/env';
 
 export type DataContextType = {
   organizations: Organizations[];
   departments: Departments[];
   orgId?: number;
   handleOrgChange: (id: number) => void;
-}
+  isDepartmentsLoading: boolean;
+};
 
 type DataProviderProps = {
   children: React.ReactNode;
-}
+};
 
-export const DataContext = createContext({} as DataContextType)
+export const DataContext = createContext({} as DataContextType);
 
 export function DataProvider({ children }: DataProviderProps) {
   const [orgId, setOrgId] = useState<number>(1);
-
   const queryClient = useQueryClient();
 
   const fetchOrganizations = useCallback(async () => {
     const response = await fetch(`${env.VITE_API_URL}/v1/organizations/`, {
       headers: {
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        Authorization: `Bearer ${localStorage.getItem('access_token')}`,
       },
     });
 
     if (!response.ok) {
-      throw new Error("Failed to fetch data");
+      throw new Error('Failed to fetch data');
     }
 
     return response.json();
   }, []);
 
   const { data: organizations = [] } = useQuery<Organizations[]>({
-    queryKey: ["organizations"],
+    queryKey: ['organizations'],
     queryFn: fetchOrganizations,
   });
 
   const fetchDepartments = useCallback(async () => {
     const response = await fetch(`${env.VITE_API_URL}/v1/departments/org/${orgId}`, {
       headers: {
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        Authorization: `Bearer ${localStorage.getItem('access_token')}`,
       },
     });
 
     if (!response.ok) {
-      throw new Error("Failed to fetch data");
+      throw new Error('Failed to fetch data');
     }
 
     return response.json();
   }, [orgId]);
 
-  const { data: departments = [] } = useQuery<Departments[]>({
-    queryKey: ["departments", orgId],
+  const { data: departments = [], isLoading: isDepartmentsLoading } = useQuery<Departments[]>({
+    queryKey: ['departments', orgId],
     queryFn: fetchDepartments,
   });
 
   const handleOrgChange = (id: number) => {
     setOrgId(id);
     queryClient.refetchQueries({
-      queryKey: ["departments", orgId],
-    })
-  }
+      queryKey: ['departments', id],
+    });
+  };
 
   return (
-    <DataContext.Provider value={{ organizations, departments, orgId, handleOrgChange }}>
+    <DataContext.Provider
+      value={{ organizations, departments, orgId, handleOrgChange, isDepartmentsLoading }}
+    >
       {children}
     </DataContext.Provider>
-  )
+  );
 }
